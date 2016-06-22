fb49f8894d60f31c317738cca7755d4ab1523bdf
R. S. Doiel <rsdoiel@gmail.com>
2016-06-22 14:57:43 -0700

    Fixed error in mk-release.sh

--- mk-release.sh
+++ mk-release.sh
@@ -2,7 +2,7 @@
 #
 # Make releases for Linux/amd64, Linux/ARM7 (Raspberry Pi), Windows, and Mac OX X (darwin)
 #
-for PROGNAME in "findfile finddir"; do
+for PROGNAME in findfile finddir; do
     env GOOS=linux GOARCH=amd64 go build -o dist/linux_amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
     env GOOS=linux GOARCH=arm GOARM=7 go build -o dist/raspberrypi/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
     env GOOS=windows GOARCH=amd64 go build -o dist/windows/$PROGNAME.exe cmds/$PROGNAME/$PROGNAME.go
