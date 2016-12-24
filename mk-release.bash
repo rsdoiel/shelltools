5acb09690ef6279eebcfad8bb1594f5c4f974ff3
R. S. Doiel <rsdoiel@gmail.com>
2016-12-24 14:32:17 -0800

    Quick Save

--- mk-release.bash
+++ mk-release.bash
@@ -4,21 +4,23 @@
 #
 PROJECT=shelltools
 
+PROG_LIST="findfile finddir mergepath pathparts range reldate timefmt urlparse"
+
 VERSION=$(grep -m1 'Version = ' $PROJECT.go | cut -d\"  -f 2)
 
 RELEASE_NAME=$PROJECT-$VERSION
 
 echo "Preparing $RELEASE_NAME"
-for PROGNAME in findfile finddir mergepath pathparts range reldate timefmt urlparse; do
+for PROGNAME in $PROG_LIST; do
     echo "Cross compiling $PROGNAME"
-    env GOOS=linux GOARCH=amd64 go build -o dist/linux-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
-    env GOOS=linux GOARCH=arm GOARM=6 go build -o dist/raspberrypi-arm6/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
-    env GOOS=linux GOARCH=arm GOARM=7 go build -o dist/raspberrypi-arm7/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
-    env GOOS=windows GOARCH=amd64 go build -o dist/windows-amd64/$PROGNAME.exe cmds/$PROGNAME/$PROGNAME.go
-    env GOOS=darwin	GOARCH=amd64 go build -o dist/macosx-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
+    env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dist/linux-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
+    env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=6 go build -o dist/raspberrypi-arm6/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
+    env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -o dist/raspberrypi-arm7/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
+    env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o dist/windows-amd64/$PROGNAME.exe cmds/$PROGNAME/$PROGNAME.go
+    env CGO_ENABLED=0 GOOS=darwin	GOARCH=amd64 go build -o dist/macosx-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
 done
 echo "Assembling dist/"
-for FNAME in README*.md LICENSE INSTALL.md; do
+for FNAME in README.md LICENSE INSTALL.md; do
     cp -v $FNAME dist/
 done
 echo "Zipping $RELEASE_NAME"
